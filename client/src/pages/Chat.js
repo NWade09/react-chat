02b6eaa80876c865a77929cb906b8ec6391205b0
Nick Wade <nick.wade183@gmail.com>
2024-11-05 22:41:44 +1000

    added auto-scroll when chat is opened and when messages are sent.

--- client/src/pages/Chat.js
+++ client/src/pages/Chat.js
@@ -2,35 +2,44 @@ import '../css/Chat.css';
 import '../css/Navbar.css';
 import Navbar from '../components/Navbar.js';
 import { ref, push, onChildAdded } from "firebase/database";
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import database from '../components/firebaseConfig';
 import { useNavigate } from 'react-router-dom';
 
 function Chat() {
+    const navigate = useNavigate();
     const sessionData = JSON.parse(sessionStorage.getItem('sessionData'));
-    const navigate = useNavigate()
-    if (!sessionData){
-        navigate('/Login');
-    }
     const [message, setMessage] = useState('');
     const [messages, setMessages] = useState([]);
+    const messagesEndRef = useRef(null);
+    if (!sessionData){
+        navigate('/Login')
+    }
+
     useEffect(() => {
         const messagesRef = ref(database, 'messages');
-    
+
         const unsubscribe = onChildAdded(messagesRef, (snapshot) => {
             const newMessage = snapshot.val();
             setMessages((prevMessages) => [...prevMessages, newMessage]);
         });
-    
-        // Clean up the listener when the component unmounts
+
         return () => unsubscribe();
     }, []);
+
+
+    useEffect(() => {
+        if (messagesEndRef.current) {
+            messagesEndRef.current.scrollIntoView({ behavior: 'smooth' });
+        }
+    }, [messages]);
 
     const sendMessage = (e) => {
         e.preventDefault();
         if (message.trim() === '') return;
 
         const messagesRef = ref(database, 'messages');
+
 
         push(messagesRef, {
             text: message,
@@ -55,6 +64,7 @@ function Chat() {
                         <div className='message-text'>{msg.text}</div>
                     </li>
                 ))}
+                <div ref={messagesEndRef} />
             </ul>
 
             <form className='input-container' onSubmit={sendMessage}>
